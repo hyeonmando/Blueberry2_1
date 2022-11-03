8e88ac3e8d37fb564a9c4c62589481fb49caf844
_recordmystory <_recordmystory@abc.com>
2022-11-03 22:38:49 +0900

    Storage 기능 추가

--- app/src/main/java/com/example/blueberry2/HomeActivity.java
+++ app/src/main/java/com/example/blueberry2/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.blueberry2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        Button movewrite = findViewById(R.id.btn_write);
+        movewrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WriteActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
